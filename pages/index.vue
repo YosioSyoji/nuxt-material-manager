250d3cc35729f476fda016257ca545a7136cc401
SYOJI <y.syoji@kura-corpo.co.jp>
2021-04-26 06:46:11 +0100

    test

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <div class="text-center">
-        <logo />
         <vuetify-logo />
       </div>
       <v-card>
